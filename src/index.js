9a4aec600f57fe188cc3f59800abd730869d36a1
Matthieu Clay <matthieu.clay@outlook.fr>
2022-03-15 17:10:59 +0100

    :sparkles: add redux component

--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@ import { render } from "react-dom";
 import App from "./App";
 import "./styles/index.scss";
 
+// REDUX
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
 render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
   document.getElementById("root")
 );
